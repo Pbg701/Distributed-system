aea3978a49b9935cdbda82a2dd8b46d35d90dbde
Prashant_Gaikwad_701 <165748489+Pbg701@users.noreply.github.com>
2025-04-28 08:22:33 +0530

    hello

--- A5Main/tokenRing.java
+++ A5Main/tokenRing.java
@@ -2,74 +2,76 @@ import java.util.Scanner;
 
 public class tokenRing {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of jode you want in ring :");
-        int n = sc.nextInt();
-        System.out.println("ring formed is as below: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(i + " ");
-
-        }
-        System.out.println();
-        int choice = 0;
-        do {
-            System.out.println("Enter Sender:");
-            int sender = sc.nextInt();
-            System.out.println("Enter Receiver");
-            int receiver = sc.nextInt();
-            System.out.println("Enter data to send:");
-            int data = sc.nextInt();
-            int token = 0;
-            System.out.println("Token passing :");
-            for (int i = token; i <= sender; i++) {
-                System.out.println(i + " -> ");
-            }
-            System.out.println("" + sender);
-            System.out.println("sender " + sender + "sending data " + data);
-            for (int i = sender + 1; i != receiver; i = (i + 1) % n) {
-                System.out.println("Data Forworded by " + i);
-            }
-            System.out.println("Receiver " + receiver + "received data " + data);
-            token = sender;
-            System.out.println("Toen is now at node " + token);
-        } while (choice == 1);
         // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter the number of nodes you want in the ring:");
-        // int n = sc.nextInt(); // Number of nodes in the ring
-        // System.out.println("Ring Formed is as below: ");
+        // System.out.println("Enter the number of node you want in ring :");
+        // int n = sc.nextInt();
+        // System.out.println("Ring formed is as below: ");
         // for (int i = 0; i < n; i++) {
         // System.out.print(i + " ");
         // }
-        // System.out.println("");
+
+        // System.out.println();
         // int choice = 0;
         // do {
         // System.out.println("Enter Sender:");
-        // int sender = sc.nextInt(); // Sender node
-        // System.out.println("Enter Receiver:");
-        // int receiver = sc.nextInt(); // Receiver node
-        // System.out.println("Enter Data to send:");
-        // String data = sc.next(); // Data to be sent
+        // int sender = sc.nextInt();
+        // System.out.println("Enter Receiver");
+        // int receiver = sc.nextInt();
+        // System.out.println("Enter data to send:");
+        // int data = sc.nextInt();
         // int token = 0;
-        // System.out.println("Token Passing:");
-        // for (int i = token; (i % n) != sender; i = (i + 1) % n) {
-        // System.out.print(i + " -> ");
+        // System.out.println("Token passing :");
+        // for (int i = token; i < sender; i++) {
+        // System.out.println(i + " -> ");
         // }
-        // System.out.println(" " + sender); // Token passed to sender
-        // System.out.println("Sender " + sender + " sending data: " + data); // Sender
-        // sending data
-        // for (int i = (sender + 1) % n; i != receiver; i = (i + 1) % n) {
-        // System.out.println("Data forwarded by " + i); // Data forwarded by
-        // intermediate nodes
+        // System.out.println(" " + sender);
+        // System.out.println("sender " + sender + " sending data " + data);
+        // for (int i = sender + 1; i != receiver; i = (i + 1) % n) {
+        // System.out.println("Data Forworded by " + i);
         // }
-        // System.out.println("Receiver " + receiver + " received data: " + data); //
-        // Receiver receiving data
-        // token = sender; // Update token position
-        // System.out.println("Token is now at node: " + token); // Token position
-        // updated
+        // System.out.println("Receiver " + receiver + " received data " + data);
+        // token = sender;
+        // System.out.println("Token is now at node " + token);
         // System.out.println("Do you want to continue? Press 1 for Yes and 0 for No:");
         // choice = sc.nextInt(); // User choice to continue or exit
         // } while (choice == 1);
-        // System.out.println("Exiting the program."); // Exit message
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of nodes you want in the ring:");
+        int n = sc.nextInt(); // Number of nodes in the ring
+        System.out.println("Ring Formed is as below: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+        int choice = 0;
+        do {
+            System.out.println("Enter Sender:");
+            int sender = sc.nextInt(); // Sender node
+            System.out.println("Enter Receiver:");
+            int receiver = sc.nextInt(); // Receiver node
+            System.out.println("Enter Data to send:");
+            int data = sc.nextInt(); // Data to be sent
+            int token = 0;
+            System.out.println("Token Passing:");
+            for (int i = token; (i % n) != sender; i = (i + 1) % n) {
+                System.out.print(i + " -> ");
+            }
+            System.out.println(" " + sender); // Token passed to sender
+            System.out.println("Sender " + sender + " sending data: " + data); // Sender
+            // sending data
+            for (int i = (sender + 1) % n; i != receiver; i = (i + 1) % n) {
+                System.out.println("Data forwarded by " + i); // Data forwarded by
+                /// intermediate nodes
+            }
+            System.out.println("Receiver " + receiver + " received data: " + data); //
+            // Receiver receiving data
+            token = sender; // Update token position
+            System.out.println("Token is now at node: " + token); // Token position
+            // updated
+            System.out.println("Do you want to continue? Press 1 for Yes and 0 for No:");
+            choice = sc.nextInt(); // User choice to continue or exit
+        } while (choice == 1);
+        System.out.println("Exiting the program."); // Exit message
 
     }
 }
